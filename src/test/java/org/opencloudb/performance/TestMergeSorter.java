544c653b86e95150316932286881922d35f98323
coder_czp <coder_czp@126.com>
2014-12-07 23:17:11 +0800

    调整测试用例的位置

--- src/test/java/org/opencloudb/performance/TestMergeSorter.java
+++ src/test/java/org/opencloudb/performance/TestMergeSorter.java
@@ -1,4 +1,4 @@
-package org.opencloudb.mpp;
+package org.opencloudb.performance;
 
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
@@ -7,23 +7,24 @@ import java.util.HashSet;
 import java.util.Random;
 import java.util.Set;
 
-import org.junit.Test;
+import org.opencloudb.mpp.ColMeta;
+import org.opencloudb.mpp.OrderCol;
+import org.opencloudb.mpp.RowDataPacketSorter;
 import org.opencloudb.mpp.tmp.FastRowDataPacketSorter;
 import org.opencloudb.mysql.BufferUtil;
 import org.opencloudb.net.mysql.RowDataPacket;
 
 public class TestMergeSorter {
 
-	@Test
-	public void testOldSorter() {
+	public static void main(String[] args) {
 		ColMeta colMeta = new ColMeta(0, ColMeta.COL_TYPE_INT);
 		OrderCol col = new OrderCol(colMeta, OrderCol.COL_ORDER_TYPE_DESC);
 		OrderCol[] orderCols = { col };
 		RowDataPacketSorter sorter = new FastRowDataPacketSorter(orderCols);
 		byte idLen = 4;
 		byte packId = 0;
-		int maxCount = 10000000;
-		int bound = maxCount * 2;
+		int maxCount = 100;
+		int bound = maxCount * 1;// *2
 		Random rd = new Random();
 		Set<Integer> set = new HashSet<Integer>();
 		while (set.size() < maxCount) {
@@ -45,20 +46,21 @@ public class TestMergeSorter {
 		}
 		set.clear();
 		System.gc();
-		System.out.println("add finished");
-		for (int i = 0; i < 100000; i++) {
+		System.out.println("add finished" + "");
+		Collection<RowDataPacket> res = null;
+		for (int i = 0; i < 100; i++) {
 			long st = System.currentTimeMillis();
-			Collection<RowDataPacket> res = sorter.getSortedResult();
+			res = sorter.getSortedResult();
 			long end = System.currentTimeMillis();// 37.246//15.196
 			System.out.println((end - st) / 1000.0);
 		}
-		// for (RowDataPacket row : res) {
-		// byte[] x = row.fieldValues.get(0);
-		// byte[] name = row.fieldValues.get(1);
-		// ByteBuffer wrap = ByteBuffer.wrap(x);
-		// wrap.order(ByteOrder.LITTLE_ENDIAN);
-		// System.out.println(wrap.getInt()+","+new String(name));
-		//
-		// }
+		for (RowDataPacket row : res) {
+			byte[] x = row.fieldValues.get(0);
+			byte[] name = row.fieldValues.get(1);
+			ByteBuffer wrap = ByteBuffer.wrap(x);
+			wrap.order(ByteOrder.LITTLE_ENDIAN);
+			System.out.println(wrap.getInt() + "," + new String(name));
+
+		}
 	}
 }
