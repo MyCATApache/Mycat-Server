7a1667cfc0c55375930d70c1296b78dd724f414d
wuzhihui <zhihui.wu@hp.com>
2014-12-15 12:24:44 +0800

    Sorter 类集合用并发集合 Test类中增加排序的测试SQL

--- src/main/java/org/opencloudb/mpp/RowDataPacketSorter.java
+++ src/main/java/org/opencloudb/mpp/RowDataPacketSorter.java
@@ -27,6 +27,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 import org.opencloudb.net.mysql.RowDataPacket;
 import org.opencloudb.util.ByteUtil;
@@ -36,7 +37,7 @@ public class RowDataPacketSorter {
 
 	protected final OrderCol[] orderCols;
 
-	private List<RowDataPacket> sorted = new ArrayList<RowDataPacket>();
+	private ConcurrentLinkedQueue<RowDataPacket> sorted = new  ConcurrentLinkedQueue<RowDataPacket>();
 	private RowDataPacket[] array, resultTemp;
 	private int p1, pr, p2;
 
