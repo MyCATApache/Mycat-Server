052c0481d99dcaf01e40b0383d21c67839015f4a
coder_czp@126.com <coder_czp@126.com>
2014-12-17 11:29:08 +0800

    排序中区分升降序

---
+++ src/main/java/org/opencloudb/mpp/tmp/HeapItf.java
@@ -0,0 +1,25 @@
+package org.opencloudb.mpp.tmp;
+
+import java.util.Vector;
+
+import org.opencloudb.net.mysql.RowDataPacket;
+
+/**
+ * 
+ * @author coderczp-2014-12-17
+ */
+public interface HeapItf {
+
+    public abstract void buildMinHeap();
+
+    public abstract RowDataPacket getRoot();
+
+    public abstract void add(RowDataPacket row);
+
+    public abstract Vector<RowDataPacket> getData();
+
+    public abstract void setRoot(RowDataPacket root);
+
+    public abstract void addIfRequired(RowDataPacket row);
+
+}
