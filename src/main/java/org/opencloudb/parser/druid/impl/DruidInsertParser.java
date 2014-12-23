9330b25c21605a8f36efb343954c64d9186e3801
wdw1206 <wdw1206@163.com>
2014-12-23 14:51:44 +0800

    druid1.0.12已发布，支持insert into...values(),()...的代码放开

--- src/main/java/org/opencloudb/parser/druid/impl/DruidInsertParser.java
+++ src/main/java/org/opencloudb/parser/druid/impl/DruidInsertParser.java
@@ -2,17 +2,23 @@ package org.opencloudb.parser.druid.impl;
 
 import java.sql.SQLNonTransientException;
 import java.sql.SQLSyntaxErrorException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.opencloudb.config.model.SchemaConfig;
 import org.opencloudb.config.model.TableConfig;
 import org.opencloudb.mysql.nio.handler.FetchStoreNodeOfChildTableHandler;
 import org.opencloudb.route.RouteResultset;
+import org.opencloudb.route.RouteResultsetNode;
+import org.opencloudb.route.function.AbstractPartitionAlgorithm;
 import org.opencloudb.route.util.RouterUtil;
 
 import com.alibaba.druid.sql.ast.SQLExpr;
 import com.alibaba.druid.sql.ast.SQLStatement;
 import com.alibaba.druid.sql.ast.expr.SQLBinaryOpExpr;
+import com.alibaba.druid.sql.ast.statement.SQLInsertStatement.ValuesClause;
 import com.alibaba.druid.sql.dialect.mysql.ast.statement.MySqlInsertStatement;
 
 public class DruidInsertParser extends DefaultDruidParser {
@@ -53,10 +59,10 @@ public class DruidInsertParser extends DefaultDruidParser {
 				
 				//批量insert
 				if(isMultiInsert(insert)) {
-					String msg = "multi insert not provided" ;
-					LOGGER.warn(msg);
-					throw new SQLNonTransientException(msg);
-//					parserBatchInsert(schema, rrs, partitionColumn, tableName, insert);//TODO 需要修改druid源码支持，先不支持，等druid项目修改了再放开
+//					String msg = "multi insert not provided" ;
+//					LOGGER.warn(msg);
+//					throw new SQLNonTransientException(msg);
+					parserBatchInsert(schema, rrs, partitionColumn, tableName, insert);
 				} else {
 					parserSingleInsert(schema, rrs, partitionColumn, tableName, insert);
 				}
@@ -180,88 +186,87 @@ public class DruidInsertParser extends DefaultDruidParser {
 		}
 	}
 	
-//	/**
-//	 * insert into .... select .... 或insert into table() values (),(),....
-//	 * @param schema
-//	 * @param rrs
-//	 * @param insertStmt
-//	 * @throws SQLNonTransientException
-//	 */
-//	private void parserBatchInsert(SchemaConfig schema, RouteResultset rrs, String partitionColumn, 
-//			String tableName, MySqlInsertStatement insertStmt) throws SQLNonTransientException {
-//		//insert into table() values (),(),....
-//		if(insertStmt.getValuesList().size() > 1) {
-//			//字段列数
-//			int columnNum = insertStmt.getColumns().size();
-//			int shardingColIndex = getSharingColIndex(insertStmt, partitionColumn);
-//			if(shardingColIndex == -1) {
-//				String msg = "bad insert sql (sharding column:"+ partitionColumn + " not provided," + insertStmt;
-//				LOGGER.warn(msg);
-//				throw new SQLNonTransientException(msg);
-//			} else {
-//				List<ValuesClause> valueClauseList = insertStmt.getValuesList();
-//				
-//				Map<Integer,List<ValuesClause>> nodeValuesMap = new HashMap<Integer,List<ValuesClause>>();
-//				TableConfig tableConfig = schema.getTables().get(tableName);
-//				AbstractPartionAlgorithm algorithm = tableConfig.getRule().getRuleAlgorithm();
-//				for(ValuesClause valueClause : valueClauseList) {
-//					if(valueClause.getValues().size() != columnNum) {
-//						String msg = "bad insert sql columnSize != valueSize:"
-//					             + columnNum + " != " + valueClause.getValues().size() 
-//					             + "values:" + valueClause;
-//						LOGGER.warn(msg);
-//						throw new SQLNonTransientException(msg);
-//					}
-//					String shardingValue = valueClause.getValues().get(shardingColIndex).toString().toUpperCase();
-//					Integer nodeIndex = algorithm.calculate(shardingValue);
-//					//没找到插入的分片
-//					if(nodeIndex == null) {
-//						String msg = "can't find any valid datanode :" + tableName 
-//								+ " -> " + partitionColumn + " -> " + shardingValue;
-//						LOGGER.warn(msg);
-//						throw new SQLNonTransientException(msg);
-//					}
-//					if(nodeValuesMap.get(nodeIndex) == null) {
-//						nodeValuesMap.put(nodeIndex, new ArrayList<ValuesClause>());
-//					}
-//					nodeValuesMap.get(nodeIndex).add(valueClause);
-//					System.out.println();
-//				}
-//				
-//				RouteResultsetNode[] nodes = new RouteResultsetNode[nodeValuesMap.size()];
-//				int count = 0;
-//				for(Map.Entry<Integer,List<ValuesClause>> node : nodeValuesMap.entrySet()) {
-//					Integer nodeIndex = node.getKey();
-//					List<ValuesClause> valuesList = node.getValue();
-//					insertStmt.setValuesList(valuesList);
-//					nodes[count++] = new RouteResultsetNode(tableConfig.getDataNodes().get(nodeIndex),
-//							rrs.getSqlType(),insertStmt.toString());
-//				}
-//				rrs.setNodes(nodes);
-//				rrs.setFinishedRoute(true);
-//			}
-//		} else if(insertStmt.getQuery() != null) { // insert into .... select ....
-//			String msg = "TODO:insert into .... select .... not supported!";
-//			LOGGER.warn(msg);
-//			throw new SQLNonTransientException(msg);
-//		}
-//	}
-//	
-//	/**
-//	 * 寻找拆分字段在 columnList中的索引
-//	 * @param insertStmt
-//	 * @param partitionColumn
-//	 * @return
-//	 */
-//	private int getSharingColIndex(MySqlInsertStatement insertStmt,String partitionColumn) {
-//		int shardingColIndex = -1;
-//		for(int i = 0; i < insertStmt.getColumns().size(); i++) {
-//			if(partitionColumn.equalsIgnoreCase(removeBackquote(insertStmt.getColumns().get(i).toString()))) {//找到分片字段
-//				shardingColIndex = i;
-//				return shardingColIndex;
-//			}
-//		}
-//		return shardingColIndex;
-//		
-//	}
+	/**
+	 * insert into .... select .... 或insert into table() values (),(),....
+	 * @param schema
+	 * @param rrs
+	 * @param insertStmt
+	 * @throws SQLNonTransientException
+	 */
+	private void parserBatchInsert(SchemaConfig schema, RouteResultset rrs, String partitionColumn, 
+			String tableName, MySqlInsertStatement insertStmt) throws SQLNonTransientException {
+		//insert into table() values (),(),....
+		if(insertStmt.getValuesList().size() > 1) {
+			//字段列数
+			int columnNum = insertStmt.getColumns().size();
+			int shardingColIndex = getSharingColIndex(insertStmt, partitionColumn);
+			if(shardingColIndex == -1) {
+				String msg = "bad insert sql (sharding column:"+ partitionColumn + " not provided," + insertStmt;
+				LOGGER.warn(msg);
+				throw new SQLNonTransientException(msg);
+			} else {
+				List<ValuesClause> valueClauseList = insertStmt.getValuesList();
+				
+				Map<Integer,List<ValuesClause>> nodeValuesMap = new HashMap<Integer,List<ValuesClause>>();
+				TableConfig tableConfig = schema.getTables().get(tableName);
+				AbstractPartitionAlgorithm algorithm = tableConfig.getRule().getRuleAlgorithm();
+				for(ValuesClause valueClause : valueClauseList) {
+					if(valueClause.getValues().size() != columnNum) {
+						String msg = "bad insert sql columnSize != valueSize:"
+					             + columnNum + " != " + valueClause.getValues().size() 
+					             + "values:" + valueClause;
+						LOGGER.warn(msg);
+						throw new SQLNonTransientException(msg);
+					}
+					String shardingValue = valueClause.getValues().get(shardingColIndex).toString().toUpperCase();
+					Integer nodeIndex = algorithm.calculate(shardingValue);
+					//没找到插入的分片
+					if(nodeIndex == null) {
+						String msg = "can't find any valid datanode :" + tableName 
+								+ " -> " + partitionColumn + " -> " + shardingValue;
+						LOGGER.warn(msg);
+						throw new SQLNonTransientException(msg);
+					}
+					if(nodeValuesMap.get(nodeIndex) == null) {
+						nodeValuesMap.put(nodeIndex, new ArrayList<ValuesClause>());
+					}
+					nodeValuesMap.get(nodeIndex).add(valueClause);
+					System.out.println();
+				}
+				
+				RouteResultsetNode[] nodes = new RouteResultsetNode[nodeValuesMap.size()];
+				int count = 0;
+				for(Map.Entry<Integer,List<ValuesClause>> node : nodeValuesMap.entrySet()) {
+					Integer nodeIndex = node.getKey();
+					List<ValuesClause> valuesList = node.getValue();
+					insertStmt.setValuesList(valuesList);
+					nodes[count++] = new RouteResultsetNode(tableConfig.getDataNodes().get(nodeIndex),
+							rrs.getSqlType(),insertStmt.toString());
+				}
+				rrs.setNodes(nodes);
+				rrs.setFinishedRoute(true);
+			}
+		} else if(insertStmt.getQuery() != null) { // insert into .... select ....
+			String msg = "TODO:insert into .... select .... not supported!";
+			LOGGER.warn(msg);
+			throw new SQLNonTransientException(msg);
+		}
+	}
+	
+	/**
+	 * 寻找拆分字段在 columnList中的索引
+	 * @param insertStmt
+	 * @param partitionColumn
+	 * @return
+	 */
+	private int getSharingColIndex(MySqlInsertStatement insertStmt,String partitionColumn) {
+		int shardingColIndex = -1;
+		for(int i = 0; i < insertStmt.getColumns().size(); i++) {
+			if(partitionColumn.equalsIgnoreCase(removeBackquote(insertStmt.getColumns().get(i).toString()))) {//找到分片字段
+				shardingColIndex = i;
+				return shardingColIndex;
+			}
+		}
+		return shardingColIndex;
+	}
 }
