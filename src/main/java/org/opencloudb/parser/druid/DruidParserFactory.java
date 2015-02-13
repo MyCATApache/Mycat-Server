4565726d385be2e019c5665859ef587c309a8568
DengLiren [邓利仁] <dengliren@xiaomi.com>
2015-02-13 15:34:36 +0800

    增加LockTable和UnlockTables语句支持

--- src/main/java/org/opencloudb/parser/druid/DruidParserFactory.java
+++ src/main/java/org/opencloudb/parser/druid/DruidParserFactory.java
@@ -1,20 +1,10 @@
 package org.opencloudb.parser.druid;
 
-import org.opencloudb.parser.druid.impl.DefaultDruidParser;
-import org.opencloudb.parser.druid.impl.DruidAlterTableParser;
-import org.opencloudb.parser.druid.impl.DruidCreateTableParser;
-import org.opencloudb.parser.druid.impl.DruidDeleteParser;
-import org.opencloudb.parser.druid.impl.DruidInsertParser;
-import org.opencloudb.parser.druid.impl.DruidSelectParser;
-import org.opencloudb.parser.druid.impl.DruidUpdateParser;
+import com.alibaba.druid.sql.dialect.mysql.ast.statement.*;
+import org.opencloudb.parser.druid.impl.*;
 
 import com.alibaba.druid.sql.ast.SQLStatement;
 import com.alibaba.druid.sql.ast.statement.SQLSelectStatement;
-import com.alibaba.druid.sql.dialect.mysql.ast.statement.MySqlAlterTableStatement;
-import com.alibaba.druid.sql.dialect.mysql.ast.statement.MySqlCreateTableStatement;
-import com.alibaba.druid.sql.dialect.mysql.ast.statement.MySqlDeleteStatement;
-import com.alibaba.druid.sql.dialect.mysql.ast.statement.MySqlInsertStatement;
-import com.alibaba.druid.sql.dialect.mysql.ast.statement.MySqlUpdateStatement;
 
 /**
  * DruidParser的工厂类
@@ -24,7 +14,7 @@ import com.alibaba.druid.sql.dialect.mysql.ast.statement.MySqlUpdateStatement;
 public class DruidParserFactory {
 	
 	public static DruidParser create(SQLStatement statement) {
-		DruidParser parser = null;
+		DruidParser parser;
 		if(statement instanceof SQLSelectStatement) {
 			parser = new DruidSelectParser();
 		} else if(statement instanceof MySqlInsertStatement) {
@@ -37,7 +27,11 @@ public class DruidParserFactory {
 			parser = new DruidUpdateParser();
 		} else if(statement instanceof MySqlAlterTableStatement) {
 			parser = new DruidAlterTableParser();
-		} else {
+		} else if (statement instanceof MySqlLockTableStatement) {
+			parser = new DruidLockTableParser();
+		} else if (statement instanceof MySqlUnlockTablesStatement) {
+			parser = new DruidUnlockTablesParser();
+		}  else {
 			parser = new DefaultDruidParser();
 		}
 		
