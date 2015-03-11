52f472d8c1b4050a2e077fed52840e93f1be5b61
magicdoom <magicdoom@gmail.com>
2015-03-11 22:54:32 +0800

    db2分页

--- src/main/java/org/opencloudb/parser/druid/impl/DruidSelectDb2Parser.java
+++ src/main/java/org/opencloudb/parser/druid/impl/DruidSelectDb2Parser.java
@@ -2,11 +2,14 @@ package org.opencloudb.parser.druid.impl;
 
 import com.alibaba.druid.sql.PagerUtils;
 import com.alibaba.druid.sql.ast.SQLExpr;
+import com.alibaba.druid.sql.ast.SQLOrderBy;
 import com.alibaba.druid.sql.ast.SQLStatement;
+import com.alibaba.druid.sql.ast.expr.SQLAggregateExpr;
 import com.alibaba.druid.sql.ast.expr.SQLBinaryOpExpr;
 import com.alibaba.druid.sql.ast.expr.SQLBinaryOperator;
 import com.alibaba.druid.sql.ast.expr.SQLIntegerExpr;
 import com.alibaba.druid.sql.ast.statement.*;
+import com.alibaba.druid.sql.dialect.db2.ast.stmt.DB2SelectQueryBlock;
 import com.alibaba.druid.sql.dialect.db2.parser.DB2StatementParser;
 import com.alibaba.druid.sql.dialect.mysql.ast.statement.MySqlSelectQueryBlock;
 import com.alibaba.druid.sql.dialect.mysql.ast.statement.MySqlSelectQueryBlock.Limit;
@@ -14,6 +17,9 @@ import com.alibaba.druid.sql.dialect.mysql.ast.statement.MySqlUnionQuery;
 import com.alibaba.druid.sql.dialect.oracle.ast.stmt.OracleSelect;
 import com.alibaba.druid.sql.dialect.oracle.ast.stmt.OracleSelectQueryBlock;
 import com.alibaba.druid.sql.dialect.oracle.parser.OracleStatementParser;
+import com.alibaba.druid.sql.dialect.sqlserver.ast.SQLServerSelect;
+import com.alibaba.druid.sql.dialect.sqlserver.ast.SQLServerSelectQueryBlock;
+import com.alibaba.druid.sql.dialect.sqlserver.parser.SQLServerStatementParser;
 import org.opencloudb.MycatServer;
 import org.opencloudb.cache.LayerCachePool;
 import org.opencloudb.config.model.SchemaConfig;
@@ -24,8 +30,8 @@ import java.sql.SQLNonTransientException;
 import java.util.List;
 import java.util.Map;
 
-public class DruidSelectDb2Parser extends DruidSelectParser {
 
+public class DruidSelectDb2Parser extends DruidSelectOracleParser {
 
 
 
