5357a8820c77a672d81629e8668f32a37e4fd57b
czn <MUXmux>
2015-03-11 13:28:52 +0800

    转换本地分页改取原始sql，否则会导致部分非mysql的库的语法错误解析，比如字符串连接符

--- src/main/java/org/opencloudb/parser/druid/impl/DruidSelectPostgresqlParser.java
+++ src/main/java/org/opencloudb/parser/druid/impl/DruidSelectPostgresqlParser.java
@@ -1,7 +1,10 @@
 package org.opencloudb.parser.druid.impl;
 
 import com.alibaba.druid.sql.PagerUtils;
+import com.alibaba.druid.sql.ast.statement.SQLSelect;
+import com.alibaba.druid.sql.ast.statement.SQLSelectQuery;
 import com.alibaba.druid.sql.ast.statement.SQLSelectStatement;
+import com.alibaba.druid.sql.dialect.postgresql.ast.stmt.PGSelectQueryBlock;
 import com.alibaba.druid.sql.dialect.postgresql.parser.PGSQLStatementParser;
 
 public class DruidSelectPostgresqlParser extends DruidSelectParser
@@ -10,10 +13,17 @@ public class DruidSelectPostgresqlParser extends DruidSelectParser
 
     protected String convertToNativePageSql(String sql, int offset, int count)
     {
-        PGSQLStatementParser oracleParser = new PGSQLStatementParser(sql);
-        SQLSelectStatement oracleStmt = (SQLSelectStatement) oracleParser.parseStatement();
-
-        return PagerUtils.limit(oracleStmt.getSelect(), "postgresql", offset, count);
+        PGSQLStatementParser pgParser = new PGSQLStatementParser(sql);
+        SQLSelectStatement pgStmt = (SQLSelectStatement) pgParser.parseStatement();
+        SQLSelect select = pgStmt.getSelect();
+        SQLSelectQuery query= select.getQuery();
+       if(query instanceof PGSelectQueryBlock)
+       {
+           PGSelectQueryBlock pgSelectQueryBlock= (PGSelectQueryBlock) query;
+           pgSelectQueryBlock.setLimit(null);
+           pgSelectQueryBlock.setOffset(null);
+       }
+        return PagerUtils.limit(select, "postgresql", offset, count);
 
     }
 
