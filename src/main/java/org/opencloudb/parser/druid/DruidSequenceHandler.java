9fa4ae8e2e10c664e88c4f8dafe41c967512a702
Ben Gong <woshiben@hotmail.com>
2015-05-18 20:17:54 +0800

    修正获取MYCAT SEQ的匹配语句

--- src/main/java/org/opencloudb/parser/druid/DruidSequenceHandler.java
+++ src/main/java/org/opencloudb/parser/druid/DruidSequenceHandler.java
@@ -52,15 +52,14 @@ public class DruidSequenceHandler {
 		return executeSql;
 	}
 
-	private String getTableName(String sql) {
+	public String getTableName(String sql) {
+		sql = sql.toUpperCase();
 		// String tableName = StringUtil.getTableName(sql).toUpperCase();
 		int beginIndex = sql.indexOf(MATCHED_FEATURE);
 		if(beginIndex == -1 || beginIndex == sql.length()) {
 			throw new RuntimeException(sql+" 中应包含语句 "+MATCHED_FEATURE);
 		}
 		
-		String tableName = sql.toUpperCase().substring(beginIndex+MATCHED_FEATURE.length()+1);
-		
-		return tableName;
+		return sql.toUpperCase().substring(beginIndex+MATCHED_FEATURE.length());
 	}
 }
