b4af36b02886f030b13b41bd3ad5ccbbbb19d2a3
magicdoom <magicdoom@gmail.com>
2015-05-24 23:23:18 +0800

    修复insert序列解析错误和序列next value for的大小写与空格支持

--- src/test/java/org/opencloudb/parser/druid/DruidSequenceHandlerTest.java
+++ src/test/java/org/opencloudb/parser/druid/DruidSequenceHandlerTest.java
@@ -5,6 +5,9 @@ import static junit.framework.Assert.*;
 import org.junit.Test;
 import org.opencloudb.config.model.SystemConfig;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * 获取MYCAT SEQ 表名。
  */
@@ -17,6 +20,19 @@ public class DruidSequenceHandlerTest {
 		String sql = "select next value for mycatseq_xxxx".toUpperCase();
 		String tableName = handler.getTableName(sql);
 		assertEquals(tableName, "XXXX");
+
+
+        sql = " insert into test(id,sid)values(next value for MYCATSEQ_TEST,1)".toUpperCase();
+         tableName = handler.getTableName(sql);
+        assertEquals(tableName, "TEST");
+
+        sql = " insert into test(id,sid)values(next value for MYCATSEQ_TEST       ,1)".toUpperCase();
+        tableName = handler.getTableName(sql);
+        assertEquals(tableName, "TEST");
+
+        sql = " insert into test(id)values(next value for MYCATSEQ_TEST  )".toUpperCase();
+        tableName = handler.getTableName(sql);
+        assertEquals(tableName, "TEST");
 	}
 
 	@Test
@@ -27,4 +43,15 @@ public class DruidSequenceHandlerTest {
 		String tableName = handler.getTableName(sql);
 		assertEquals(tableName, "XXXX");
 	}
+
+    public static void main(String[] args)
+    {
+        String patten="(?:(\\s*next\\s+value\\s+for\\s*MYCATSEQ_(\\w+))(,|\\)|\\s)*)+";
+        Pattern pattern = Pattern.compile(patten,Pattern.CASE_INSENSITIVE);
+         String sql="insert into test(id,sid)values(    next    value    for        MYCATSEQ_TEST ,1)";
+          Matcher matcher = pattern.matcher(sql);
+        System.out.println(matcher.find());
+        System.out.println(matcher.group(1));
+        System.out.println(matcher.group(2));
+    }
 }
