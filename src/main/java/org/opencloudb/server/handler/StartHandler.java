64de710b4dbf89b01e089f09ce7b1e97e01cb5fc
magicdoom <magicdoom@gmail.com>
2015-06-03 19:52:34 +0800

    支持start transaction与begin开启事务命令

--- src/main/java/org/opencloudb/server/handler/StartHandler.java
+++ src/main/java/org/opencloudb/server/handler/StartHandler.java
@@ -32,11 +32,16 @@ import org.opencloudb.server.parser.ServerParseStart;
  * @author mycat
  */
 public final class StartHandler {
-
+    private static final byte[] AC_OFF = new byte[] { 7, 0, 0, 1, 0, 0, 0, 0,
+            0, 0, 0 };
     public static void handle(String stmt, ServerConnection c, int offset) {
         switch (ServerParseStart.parse(stmt, offset)) {
         case ServerParseStart.TRANSACTION:
-            c.writeErrMessage(ErrorCode.ER_UNKNOWN_COM_ERROR, "Unsupported statement");
+            if (c.isAutocommit())
+            {
+                c.setAutocommit(false);
+            }
+            c.write(c.writeToBuffer(AC_OFF, c.allocate()));
             break;
         default:
             c.execute(stmt, ServerParse.START);
