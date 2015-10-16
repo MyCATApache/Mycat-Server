4725044a4e7b4cb8b155b7b514dcd74351479cc0
顽石神 <250721835@qq.com>
2015-10-16 13:43:24 +0800

    Revert "提交才能切换版本"
    This reverts commit f7a742a1aab58c5317d0ca37956be71a3b6cd3a5.

--- src/main/java/org/opencloudb/config/Versions.java
+++ src/main/java/org/opencloudb/config/Versions.java
@@ -32,6 +32,6 @@ public interface Versions {
     public static final byte PROTOCOL_VERSION = 10;
 
     /**服务器版**/
-    public static final byte[] SERVER_VERSION = "5.5.8-mycat-1.4-RELEASE-20150930075207".getBytes();
+    public static final byte[] SERVER_VERSION = "5.5.8-mycat-1.4-alpha-20150520235658".getBytes();
 
 }
