2c8d790ef28dee97accbf3fe829118cd514258e7
fireflyhoo <fireflyhoo@gmail.com>
2015-11-04 21:43:59 +0800

    ---提交...

--- src/main/java/io/mycat/backend/postgresql/PostgresqlKnightriders.java
+++ src/main/java/io/mycat/backend/postgresql/PostgresqlKnightriders.java
@@ -3,8 +3,13 @@ package io.mycat.backend.postgresql;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.TimeZone;
@@ -12,13 +17,10 @@ import java.util.TimeZone;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.mysql.jdbc.Buffer;
-
 import io.mycat.backend.postgresql.packet.AuthenticationPacket;
 import io.mycat.backend.postgresql.packet.AuthenticationPacket.AuthType;
 import io.mycat.backend.postgresql.packet.PasswordMessage;
 import io.mycat.backend.postgresql.packet.PostgreSQLPacket;
-import io.mycat.backend.postgresql.utils.MD5Digest;
 import io.mycat.backend.postgresql.utils.PostgreSQLIOUtils;
 
 /*************
@@ -32,10 +34,10 @@ public class PostgresqlKnightriders {
 	private static Logger logger = LoggerFactory.getLogger(PostgresqlKnightriders.class);
 
 	public static void main(String[] args) {
-		
+
 		List<String[]> paramList = new ArrayList<String[]>();
 		String user = "postgres";
-		String password  =  "coollf";
+		String password = "coollf";
 		String database = "odoo";
 		String appName = "MyCat-Server";
 		String assumeMinServerVersion = "9.0.0";
@@ -47,32 +49,57 @@ public class PostgresqlKnightriders {
 		paramList.add(new String[] { "TimeZone", createPostgresTimeZone() });
 		paramList.add(new String[] { "extra_float_digits", "3" });
 		paramList.add(new String[] { "application_name", appName });
-		
+
+		boolean nio = true;
+
 		try {
 			Socket socket = new Socket("localhost", 5432);
-			sendStartupPacket(socket, paramList.toArray(new String[0][]));
-			PostgreSQLPacket packet = rec(socket);
-			if(packet instanceof AuthenticationPacket){
-			  AuthType aut = ((AuthenticationPacket) packet).getAuthType();
-			  if(aut != AuthType.Ok){
-				  PasswordMessage pak = new PasswordMessage(user,password,aut,((AuthenticationPacket) packet).getSalt());
-				  ByteBuffer buffer = ByteBuffer.allocate(1024);
-				  pak.write(buffer );
-				  socket.getOutputStream().write(buffer.array());
-				  
-				  packet = rec(socket);
-				  if(packet instanceof AuthenticationPacket && ((AuthenticationPacket) packet).getAuthType() == AuthType.Ok){
-					  System.out.println("登入成功啦.....");
-				  }
-			  }
-			}
+			if (nio) {
 
+				SocketChannel channel = SocketChannel.open(new InetSocketAddress("localhost", 5432));
+
+				channel.configureBlocking(false);
+
+				// 打开并注册选择器到信道
+				Selector selector = Selector.open();
+				channel.register(selector, SelectionKey.OP_READ  | SelectionKey.OP_WRITE);
+			
+				// 启动读取线程
+				new TCPClientReadThread(selector);
+
+			//	sendStartupPacket(channel, paramList.toArray(new String[0][]));
+				ByteBuffer in = ByteBuffer.allocate(10);
+				channel.read(in);
+				System.out.println(in);
+
+			} else {
+				sendStartupPacket(socket, paramList.toArray(new String[0][]));
+				PostgreSQLPacket packet = rec(socket);
+				if (packet instanceof AuthenticationPacket) {
+					AuthType aut = ((AuthenticationPacket) packet).getAuthType();
+					if (aut != AuthType.Ok) {
+						PasswordMessage pak = new PasswordMessage(user, password, aut,
+								((AuthenticationPacket) packet).getSalt());
+						ByteBuffer buffer = ByteBuffer.allocate(1024);
+						pak.write(buffer);
+						socket.getOutputStream().write(buffer.array());
+
+						packet = rec(socket);
+						if (packet instanceof AuthenticationPacket
+								&& ((AuthenticationPacket) packet).getAuthType() == AuthType.Ok) {
+							System.out.println("登入成功啦.....");
+						}
+					}
+				}
+			}
+			
+			System.in.read();
+			System.in.read();
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-
-
 
 	private static PostgreSQLPacket rec(Socket socket) throws IOException, IllegalAccessException {
 		ByteBuffer buffer = ByteBuffer.allocateDirect(10);
@@ -156,4 +183,142 @@ public class PostgresqlKnightriders {
 		sout.write(buffer.array());
 	}
 
+	
+
+}
+
+class TCPClientReadThread implements Runnable {
+
+	private static Logger logger = LoggerFactory.getLogger(TCPClientReadThread.class);
+	private Selector selector;
+
+	public TCPClientReadThread(Selector selector) {
+		this.selector = selector;
+
+		new Thread(this).start();
+	}
+
+	public void run() {
+		boolean a = false;
+		try {
+			while (selector.select() > 0) {
+				// 遍历每个有可用IO操作Channel对应的SelectionKey
+				for (SelectionKey sk : selector.selectedKeys()) {
+
+					if(sk.isWritable()){
+						SocketChannel sc = (SocketChannel) sk.channel();
+						if(!a){
+							sendStartupPacket(sc);
+							a= true;
+						}
+					}else if (sk.isReadable()) {
+						// 使用NIO读取Channel中的数据
+						SocketChannel sc = (SocketChannel) sk.channel();
+						ByteBuffer buffer = ByteBuffer.allocate(1024);
+						sc.read(buffer);
+						buffer.flip();
+
+						// 将字节转化为为UTF-16的字符串
+						String receivedString = Charset.forName("UTF-18").newDecoder().decode(buffer).toString();
+
+						// 控制台打印出来
+						System.out.println("接收到来自服务器" + sc.socket().getRemoteSocketAddress() + "的信息:" + receivedString);
+
+						// 为下一次读取作准备
+						sk.interestOps(SelectionKey.OP_READ);
+					}
+
+					// 删除正在处理的SelectionKey
+					selector.selectedKeys().remove(sk);
+				}
+			}
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+	private static void sendStartupPacket(SocketChannel socketChannel) throws IOException {
+		List<String[]> paramList = new ArrayList<String[]>();
+		String user = "postgres";
+		String password = "coollf";
+		String database = "odoo";
+		String appName = "MyCat-Server";
+		String assumeMinServerVersion = "9.0.0";
+
+		paramList.add(new String[] { "user", user });
+		paramList.add(new String[] { "database", database });
+		paramList.add(new String[] { "client_encoding", "UTF8" });
+		paramList.add(new String[] { "DateStyle", "ISO" });
+		paramList.add(new String[] { "TimeZone", createPostgresTimeZone() });
+		paramList.add(new String[] { "extra_float_digits", "3" });
+		paramList.add(new String[] { "application_name", appName });
+
+		 String[][] params= paramList.toArray(new String[0][]);
+	
+		if (logger.isDebugEnabled()) {
+			StringBuilder details = new StringBuilder();
+			for (int i = 0; i < params.length; ++i) {
+				if (i != 0)
+					details.append(", ");
+				details.append(params[i][0]);
+				details.append("=");
+				details.append(params[i][1]);
+			}
+			logger.debug(" FE=> StartupPacket(" + details + ")");
+		}
+
+		/*
+		 * Precalculate message length and encode params.
+		 */
+		int length = 4 + 4;
+		byte[][] encodedParams = new byte[params.length * 2][];
+		for (int i = 0; i < params.length; ++i) {
+			encodedParams[i * 2] = params[i][0].getBytes("UTF-8");
+			encodedParams[i * 2 + 1] = params[i][1].getBytes("UTF-8");
+			length += encodedParams[i * 2].length + 1 + encodedParams[i * 2 + 1].length + 1;
+		}
+
+		length += 1; // Terminating \0
+
+		ByteBuffer buffer = ByteBuffer.allocate(length);
+
+		/*
+		 * Send the startup message.
+		 */
+		PostgreSQLIOUtils.SendInteger4(length, buffer);
+		PostgreSQLIOUtils.SendInteger2(3, buffer); // protocol major
+		PostgreSQLIOUtils.SendInteger2(0, buffer); // protocol minor
+		for (byte[] encodedParam : encodedParams) {
+			PostgreSQLIOUtils.Send(encodedParam, buffer);
+			PostgreSQLIOUtils.SendChar(0, buffer);
+		}
+		buffer.flip();
+		socketChannel.write(buffer);
+
+	}
+	
+	/**
+	 * Convert Java time zone to postgres time zone. All others stay the same
+	 * except that GMT+nn changes to GMT-nn and vise versa.
+	 * 
+	 * @return The current JVM time zone in postgresql format.
+	 */
+	private static String createPostgresTimeZone() {
+		String tz = TimeZone.getDefault().getID();
+		if (tz.length() <= 3 || !tz.startsWith("GMT")) {
+			return tz;
+		}
+		char sign = tz.charAt(3);
+		String start;
+		if (sign == '+') {
+			start = "GMT-";
+		} else if (sign == '-') {
+			start = "GMT+";
+		} else {
+			// unknown type
+			return tz;
+		}
+
+		return start + tz.substring(4);
+	}
 }
