c0789295d6a4bc9d0fbed852a4dd14845b7ecca0
songwie <songwie@xrpension.com>
2015-11-30 10:37:29 +0800

    暂时去掉zk 启动加载

---
+++ src/main/java/org/opencloudb/config/ZkConfig.java
@@ -0,0 +1,17 @@
+package org.opencloudb.config;
+
+import demo.catlets.ZkCreate;
+
+/**
+ * Created by StoneGod on 2015/11/23.
+ */
+public class ZkConfig {
+	private ZkConfig(){}
+	
+	public synchronized static ZkConfig instance(){
+		return new ZkConfig();
+	}
+    public void initZk(){
+    	//ZkCreate.main(null);
+    }
+}
