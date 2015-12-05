bf047688987022e00ff6d01ba7590327ed70043d
magicdoom <magicdoom@gmail.com>
2015-12-05 20:18:17 +0800

    支持mysql jdbc 5.1.37

--- src/main/java/io/mycat/server/response/SelectVariables.java
+++ src/main/java/io/mycat/server/response/SelectVariables.java
@@ -36,6 +36,7 @@ import io.mycat.server.packet.util.PacketUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -53,7 +54,7 @@ public final class SelectVariables
 
      String subSql=   sql.substring(sql.indexOf("SELECT")+6);
     List<String>  splitVar=   Splitter.on(",").omitEmptyStrings().trimResults().splitToList(subSql) ;
-
+        splitVar=convert(splitVar);
         int FIELD_COUNT = splitVar.size();
         ResultSetHeaderPacket header = PacketUtil.getHeader(FIELD_COUNT);
         FieldPacket[] fields = new FieldPacket[FIELD_COUNT];
@@ -113,7 +114,27 @@ public final class SelectVariables
     }
 
 
+    private static List<String> convert(List<String> in)
+    {
+        List<String> out=new ArrayList<>();
+        for (String s : in)
+        {
+            int asIndex=s.toUpperCase().indexOf(" AS ");
+            if(asIndex!=-1)
+            {
+                out.add(s.substring(asIndex+4)) ;
+            }
+        }
+        if(out.isEmpty())
+        {
+            return in;
+        }  else
+        {
+            return out;
+        }
 
+
+    }
 
     private static final Map<String, String> variables = new HashMap<String, String>();
     static {
@@ -136,6 +157,27 @@ public final class SelectVariables
         variables.put("@@tx_isolation", "REPEATABLE-READ");
         variables.put("@@wait_timeout", "172800");
         variables.put("@@session.auto_increment_increment", "1");
+
+        //for  jdbc driver 5.1.37
+        variables.put("character_set_client", "utf8");
+        variables.put("character_set_connection", "utf8");
+        variables.put("character_set_results", "utf8");
+        variables.put("character_set_server", "utf8");
+        variables.put("init_connect", "");
+        variables.put("interactive_timeout", "172800");
+        variables.put("license", "GPL");
+        variables.put("lower_case_table_names", "1");
+        variables.put("max_allowed_packet", "16777216");
+        variables.put("net_buffer_length", "16384");
+        variables.put("net_write_timeout", "60");
+        variables.put("query_cache_size", "0");
+        variables.put("query_cache_type", "OFF");
+        variables.put("sql_mode", "STRICT_TRANS_TABLES");
+        variables.put("system_time_zone", "CST");
+        variables.put("time_zone", "SYSTEM");
+        variables.put("tx_isolation", "REPEATABLE-READ");
+        variables.put("wait_timeout", "172800");
+        variables.put("auto_increment_increment", "1");
     }
     
 
