a96299e3ca3811460da3e6231252776507be1519
yuanfang <digdeep@126.com>
2016-01-11 15:48:26 +0800

    重构 HintHandlerFactory，fix原有的多次重复初始化，不断new HintHander的问题；以及是多线程的并发的安全问题

--- src/main/java/org/opencloudb/route/handler/HintHandlerFactory.java
+++ src/main/java/org/opencloudb/route/handler/HintHandlerFactory.java
@@ -4,12 +4,14 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class HintHandlerFactory {
-	
-	private static volatile boolean isInit = false;
-	
 	 //sql注释的类型处理handler 集合，现在支持两种类型的处理：sql,schema
-    private static Map<String,HintHandler> hintHandlerMap = new HashMap<String,HintHandler>();
+    private static Map<String,HintHandler> hintHandlerMap = null;
 
+    static{
+    	hintHandlerMap = new HashMap<String,HintHandler>();
+    	init();	// fix原有的多次重复初始化，不断new HintHander的问题；以及是多线程的并发的安全问题; digdeep@126.com
+    }
+    
     private HintHandlerFactory() {
     }
     
@@ -18,17 +20,14 @@ public class HintHandlerFactory {
         hintHandlerMap.put("schema",new HintSchemaHandler());
         hintHandlerMap.put("datanode",new HintDataNodeHandler());
         hintHandlerMap.put("catlet",new HintCatletHandler());
-        isInit = true;	// 修复多次初始化的bug
+        
+        // 新增sql hint（注解）/*#mycat:db_type=master*/ 和 /*#mycat:db_type=slave*/
+        // 该hint可以和 /*balance*/ 一起使用
+        // 实现强制走 master 和 强制走 slave
+        hintHandlerMap.put("db_type", new HintMasterDBHandler());
     }
     
-    // 双重校验锁 fix 线程安全问题
     public static HintHandler getHintHandler(String hintType) {
-    	if(!isInit) {
-    		synchronized(HintHandlerFactory.class){
-    			if(!isInit)
-    				init();
-    		}
-    	}
     	return hintHandlerMap.get(hintType);
     }
     
