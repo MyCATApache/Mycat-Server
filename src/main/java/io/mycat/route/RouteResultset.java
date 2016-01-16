74f625dea6a31189fd0dc1e680ac399dd9ac585e
yuanfang <digdeep@126.com>
2016-01-16 20:28:50 +0800

    fix bug: 全局表使用不带列名的insert语句时报错：Unknown column '_mycat_op_time' in 'field list'

--- src/main/java/io/mycat/route/RouteResultset.java
+++ src/main/java/io/mycat/route/RouteResultset.java
@@ -66,8 +66,7 @@ public final class RouteResultset implements Serializable {
     //是否可以在从库运行,此属性主要供RouteResultsetNode获取
     private Boolean canRunInReadDB;
 
-	// 强制走 master，可以通过 RouteResultset的属性canRunInReadDB
-	// 传给 RouteResultsetNode 来实现，但是 强制走 slave必须增加一个属性来实现:
+	// 强制走 master，强制走 slave统一使用该属性来标志，true走slave，false走master
 	private Boolean runOnSlave = null;	// 默认null表示不施加影响
 	
     public boolean isLoadData()
