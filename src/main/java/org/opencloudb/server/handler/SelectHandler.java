f61d3dda5b33da848b1f8ad3d51c07e3b01e88b8
magicdoom <magicdoom@gmail.com>
2016-06-02 23:02:56 +0800

    tx_read_only支持

--- src/main/java/org/opencloudb/server/handler/SelectHandler.java
+++ src/main/java/org/opencloudb/server/handler/SelectHandler.java
@@ -24,6 +24,7 @@
 package org.opencloudb.server.handler;
 
 import org.opencloudb.parser.util.ParseUtil;
+import org.opencloudb.response.SelectTxReadOnly;
 import org.opencloudb.server.ServerConnection;
 import org.opencloudb.server.parser.ServerParse;
 import org.opencloudb.server.parser.ServerParseSelect;
@@ -98,6 +99,9 @@ public final class SelectHandler {
             case ServerParseSelect.SELECT_VAR_ALL:
                 SelectVariables.execute(c,stmt);
                 break;
+			case ServerParseSelect.SESSION_TX_READ_ONLY:
+				SelectTxReadOnly.response(c);
+				break;
 		default:
 			c.execute(stmt, ServerParse.SELECT);
 		}
