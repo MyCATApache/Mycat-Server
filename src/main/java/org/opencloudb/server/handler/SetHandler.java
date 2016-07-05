17db73cd6765eaa70b948bbaad724881aa13c694
magicdoom <magicdoom@gmail.com>
2016-07-05 20:47:57 +0800

    支持rails的set names 语句

--- src/main/java/org/opencloudb/server/handler/SetHandler.java
+++ src/main/java/org/opencloudb/server/handler/SetHandler.java
@@ -113,11 +113,37 @@ public final class SetHandler {
 		}
 		case NAMES:
 			String charset = stmt.substring(rs >>> 8).trim();
+			int index=	charset.indexOf(",")  ;
+			if(index>-1) {
+				//支持rails框架自动生成的SET NAMES utf8,  @@SESSION.sql_auto_is_null = 0, @@SESSION.wait_timeout = 2147483, @@SESSION.sql_mode = 'STRICT_ALL_TABLES'
+				charset=charset.substring(0,index)	;
+			}
+			if(charset.startsWith("'")&&charset.endsWith("'"))
+			{
+				charset=charset.substring(1,charset.length()-1)  ;
+			}
 			if (c.setCharset(charset)) {
 				c.write(c.writeToBuffer(OkPacket.OK, c.allocate()));
 			} else {
-				c.writeErrMessage(ErrorCode.ER_UNKNOWN_CHARACTER_SET,
-						"Unknown charset '" + charset + "'");
+
+				/**
+				 * TODO：修复 phpAyAdmin's 的发包问题
+				 * 如： SET NAMES 'utf8' COLLATE 'utf8_general_ci' 错误
+				 */
+				int beginIndex = stmt.toLowerCase().indexOf("names");
+				int endIndex = stmt.toLowerCase().indexOf("collate");
+				if ( beginIndex > -1 && endIndex > -1 ) {
+					charset = stmt.substring(beginIndex + "names".length(), endIndex);
+					//重试一次
+					if (c.setCharset( charset.trim() )) {
+						c.write(c.writeToBuffer(OkPacket.OK, c.allocate()));
+					} else {
+						c.writeErrMessage(ErrorCode.ER_UNKNOWN_CHARACTER_SET, "Unknown charset '" + charset + "'");
+					}
+
+				} else {
+					c.writeErrMessage(ErrorCode.ER_UNKNOWN_CHARACTER_SET, "Unknown charset '" + charset + "'");
+				}
 			}
 			break;
 		case CHARACTER_SET_CLIENT:
