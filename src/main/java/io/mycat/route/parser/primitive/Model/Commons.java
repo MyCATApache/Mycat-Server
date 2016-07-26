0d9bba18bac9c53bc0cedf6e892ecf9cc5be3604
HashZhang <zhanggaoxin@sf-express.com>
2016-07-26 16:28:33 +0800

    增加函数解析方法和相关测试类

---
+++ src/main/java/io/mycat/route/parser/primitive/Model/Commons.java
@@ -0,0 +1,16 @@
+package io.mycat.route.parser.primitive.Model;
+
+/**
+ * @author Hash Zhang
+ * @version 1.0.0
+ * @date 2016/7/26
+ */
+public class Commons {
+    public final static char ARGUMENT_SEPARATOR = ',';
+    public final static char DEPENDENCY_SEPARATOR = '.';
+    public final static char LEFT_BRACKET = '(';
+    public final static char RIGHT_BRACKET = ')';
+    public final static char SLASH = '\\';
+    public final static char QUOTE = '\'';
+    public final static char DOUBLE_QUOTE = '\"';
+}
