f15e8dcbc6c48239ad09de2f30bd958f5206b028
ZzzCrazyPig <18825111236@163.com>
2016-09-08 15:53:49 +0800

    feat : improve mycat prepare
    support two prepare statement process:
    1. COM_STMT_SEND_LONG_DATA
    2. COM_STMT_RESET

--- src/main/java/io/mycat/net/handler/FrontendCommandHandler.java
+++ src/main/java/io/mycat/net/handler/FrontendCommandHandler.java
@@ -86,6 +86,14 @@ public class FrontendCommandHandler implements NIOHandler
                 commands.doStmtPrepare();
                 source.stmtPrepare(data);
                 break;
+            case MySQLPacket.COM_STMT_SEND_LONG_DATA:
+            	commands.doStmtSendLongData();
+            	source.stmtSendLongData(data);
+            	break;
+            case MySQLPacket.COM_STMT_RESET:
+            	commands.doStmtReset();
+            	source.stmtReset(data);
+            	break;
             case MySQLPacket.COM_STMT_EXECUTE:
                 commands.doStmtExecute();
                 source.stmtExecute(data);
