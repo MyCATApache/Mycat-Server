5fe05f6ad02b1d39a7a6667eb0f012efa3a9389e
547557645@qq.com <547557645@qq.com>
2016-09-26 12:24:47 +0800

    修正ShowHandler没有import  ShowDirectMemory

--- src/main/java/io/mycat/manager/handler/ShowHandler.java
+++ src/main/java/io/mycat/manager/handler/ShowHandler.java
@@ -62,6 +62,7 @@ import io.mycat.manager.response.ShowTime;
 import io.mycat.manager.response.ShowVariables;
 import io.mycat.manager.response.ShowVersion;
 import io.mycat.manager.response.ShowWhiteHost;
+import io.mycat.manager.response.ShowDirectMemory;
 import io.mycat.route.parser.ManagerParseShow;
 import io.mycat.route.parser.util.ParseUtil;
 import io.mycat.server.handler.ShowCache;
