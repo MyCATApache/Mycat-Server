a299bc647957b9eea64c3968519816264fcd624c
862911 <zhanggaoxin@sf-express.com>
2016-12-08 09:33:09 +0800

    增加常用的字符串通用方法和随机算法

--- src/main/java/io/mycat/util/StringUtil.java
+++ src/main/java/io/mycat/util/StringUtil.java
@@ -30,6 +30,8 @@ import io.mycat.sqlengine.mpp.LoadData;
 
 import java.io.ByteArrayOutputStream;
 import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Random;
 
 /**
@@ -562,6 +564,35 @@ public class StringUtil {
 			return sb.toString();
 		}
 		return "";
+	}
+
+	public static String makeString(Object... args) {
+		StringBuilder stringBuilder = new StringBuilder();
+		for (Object arg : args) {
+			stringBuilder.append(arg);
+		}
+		return stringBuilder.toString();
+	}
+
+	public static boolean isNull(String src) {
+		if (src == null || src.trim().equals("") || src.trim().equalsIgnoreCase("undefined")) {
+			return true;
+		}
+		return false;
+	}
+
+	public static String sha1(String data) throws NoSuchAlgorithmException {
+		MessageDigest md = MessageDigest.getInstance("SHA1");
+		md.update(data.getBytes());
+		StringBuffer buf = new StringBuffer();
+		byte[] bits = md.digest();
+		for (int i = 0; i < bits.length; i++) {
+			int a = bits[i];
+			if (a < 0) a += 256;
+			if (a < 16) buf.append("0");
+			buf.append(Integer.toHexString(a));
+		}
+		return buf.toString();
 	}
 
 	public static void main(String[] args) {
