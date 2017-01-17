92cc0866bce155dd757fbe5f7573493acb0638e2
kkzfl <546064298@qq.com>
2017-01-17 21:04:15 +0800

    进行zk的操作，以能实现从命令行中执行命令更新zk数据

--- src/main/java/io/mycat/manager/handler/ZKHandler.java
+++ src/main/java/io/mycat/manager/handler/ZKHandler.java
@@ -1,5 +1,6 @@
 package io.mycat.manager.handler;
 
+import io.mycat.config.loader.zkprocess.zktoxml.ZktoXmlMain;
 import io.mycat.manager.ManagerConnection;
 
 /**
@@ -10,7 +11,35 @@ import io.mycat.manager.ManagerConnection;
  * @time 23:35 2016/5/7
  */
 public class ZKHandler {
+
+    /**
+     * 直接从zk拉所有配置，然后本地执行reload_all
+     */
+    private static final String RELOAD_FROM_ZK = "zk reload_from_zk";
+
+    /**
+     * 一个是强制所有mycat reload from zk 一下
+     */
+    private static final String RELOAD_ALL = "zk reload_all_mycat_from_zk";
+
     public static void handle(String stmt, ManagerConnection c, int offset) {
+        String command = stmt.toLowerCase();
+        // 检查当前的命令是否为zk reload_from_zk
+        if (RELOAD_FROM_ZK.equals(command)) {
+            // 调用zktoxml操作
+            try {
+                ZktoXmlMain.loadZktoFile();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            // 执行重新加载本地配制信息
+            ReloadHandler.handle("RELOAD @@CONFIG", c, 7 >>> 8);
+
+            offset += RELOAD_FROM_ZK.length();
+        }
+
+        // reload_all_mycat_from_zk
 
     }
 }
