01a1f84fbd55390357f1fe6977c1241c2cae5776
YunaiV <zhijiantianya@gmail.com>
2017-05-23 00:48:30 +0800

    + MyCat 分片调试环境搭建

---
+++ src/test/java/debugger/MycatStartupTest.java
@@ -0,0 +1,16 @@
+package debugger;
+
+import io.mycat.MycatStartup;
+
+/**
+ * {@link io.mycat.MycatStartup}测试
+ *
+ * Created by yunai on 2017/5/22.
+ */
+public class MycatStartupTest {
+
+    public static void main(String[] args) {
+        MycatStartup.main(args);
+    }
+
+}
