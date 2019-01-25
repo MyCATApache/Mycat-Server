66360397b8d9f5045a402c09e404de2a8b4cecf5
maxiaoguang <mxg@maxiaoguang.com>
2019-01-25 14:17:17 +0800

    show @@white返回带通配符的ip

--- src/main/java/io/mycat/manager/response/ShowWhiteHost.java
+++ src/main/java/io/mycat/manager/response/ShowWhiteHost.java
@@ -4,6 +4,7 @@ import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -78,6 +79,22 @@ public final class ShowWhiteHost {
             row.packetId = ++packetId;
             buffer = row.write(buffer, c,true);			
 		}
+        Map<Pattern, List<UserConfig>> map2=MycatServer.getInstance().getConfig().getFirewall().getWhitehostMask();
+        for (Pattern key : map2.keySet()) {
+            List<UserConfig> userConfigs=map2.get(key);
+            String users="";
+            for (int i = 0; i < userConfigs.size(); i++) {
+                if(i>0) {
+                    users += "," + userConfigs.get(i).getName();
+                }
+                else {
+                    users += userConfigs.get(i).getName();
+                }
+            }
+            RowDataPacket row = getRow(FirewallConfig.getHost(key), users, c.getCharset());
+            row.packetId = ++packetId;
+            buffer = row.write(buffer, c,true);
+        }
 		
         // write last eof
         EOFPacket lastEof = new EOFPacket();
