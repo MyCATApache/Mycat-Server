d62d532463df233405db3ed20808357ba223d504
cui-ying <tsuiying@126.com>
2019-05-14 15:48:41 +0800

    Versions.java æ文件编码由GBK 改为了UTF-8

--- src/main/java/io/mycat/config/Versions.java
+++ src/main/java/io/mycat/config/Versions.java
@@ -31,7 +31,7 @@ public abstract class Versions {
     /**协议版本**/
     public static final byte PROTOCOL_VERSION = 10;
 
-    /**服务器版�?**/
+    /**服务器版本**/
     public static byte[] SERVER_VERSION = "5.6.29-mycat-1.6.7.1-release-20190507113837".getBytes();
 
     public static void setServerVersion(String version) {
