df0214982707d4181f86f83aad3910fa793941e7
cjw <jamie12221@outlook.com>
2019-11-18 17:01:21 +0800

    ignoreUnknownCommand

--- src/main/java/io/mycat/config/Versions.java
+++ src/main/java/io/mycat/config/Versions.java
@@ -32,7 +32,7 @@ public abstract class Versions {
     public static final byte PROTOCOL_VERSION = 10;
 
     /**服务器版本**/
-    public static byte[] SERVER_VERSION = "5.6.29-mycat-1.6.7.4-test-20191114135626".getBytes();
+    public static byte[] SERVER_VERSION = "5.6.29-mycat-1.6.7.4-test-20191118163027".getBytes();
 
     public static void setServerVersion(String version) {
         byte[] mysqlVersionPart = version.getBytes();
