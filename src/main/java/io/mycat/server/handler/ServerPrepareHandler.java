32b19538ca20bce7efa4d0f1334d214fa34c130c
funnyAnt <kaikai149@163.com>
2020-10-01 20:42:47 +0800

    #2591 prepare语句里面设置blob字段值，第一次执行的blob值会带入第二次执行的值里面

--- src/main/java/io/mycat/server/handler/ServerPrepareHandler.java
+++ src/main/java/io/mycat/server/handler/ServerPrepareHandler.java
@@ -183,6 +183,8 @@ public class ServerPrepareHandler implements FrontendPrepareHandler {
             if (LOGGER.isDebugEnabled()) {
                 LOGGER.debug("execute prepare sql: " + sql);
             }
+
+            pstmt.resetLongData();
             source.query(sql);
         }
     }
